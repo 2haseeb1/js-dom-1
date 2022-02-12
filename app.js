5b1f8731e15b1f211e7742aae86c96db6c17e0d6
Nhasib0091 <mdhaseeb079@gmail.com>
2022-02-12 14:11:20 +0600

    my-first

---
+++ app.js
@@ -0,0 +1,13 @@
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+
+var sentence = "I am hardworking. I am determined. I will be a web developer.";
+var count = 0;
+for(var i = 0; i < sentence.length; i++){
+  var letter = sentence[i];
+  if(letter=='a'){
+    count++;
+  }
+}
+console.log(count);
